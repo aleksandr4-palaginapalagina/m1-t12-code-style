acc3325d4c7e46ea03ae50b310453b9b906c5ac3
Александра <aleksandra-palagina@mail.ru>
2023-04-05 17:08:02 +0300

    задание на код стайл

--- src/DepositCalculator.java
+++ src/DepositCalculator.java
@@ -1,21 +1,28 @@
-import
+import java.util.Scanner;
 
-public class FinancialCalculator {
-    double calculateComplexPercentFunction(double amount, double yearRate, int period) {
+public class DepositCalculator {
+
+    public static void main(String[] args) {
+        new DepositCalculator().calculateContributionResult();
+    }
+
+    double calculateComplexPercent(double amount, double yearRate, int period) {
         double pay;
         double result;
+        int places = 2;
 
         pay = amount * Math.pow((1 + yearRate / 12), 12 * period);
-        result = calculateResult(pay, 2);
+        result = calculateResult(pay, places);
         return result;
     }
 
-    double calculateSimplePercentFunction(double amount, double yearRate, int depositPeriod) {
+    double calculateSimplePercent(double amount, double yearRate, int depositPeriod) {
         double pay;
         double result;
+        int places = 2;
 
         pay = amount + amount * yearRate * depositPeriod;
-        result = calculateResult(pay, 2);
+        result = calculateResult(pay, places);
         return result;
     }
 
@@ -32,6 +39,7 @@ public class FinancialCalculator {
         int period;
         int depositType;
         double depositAmount;
+        double yearRate = 0.06;
 
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите сумму вклада в рублях:");
@@ -43,15 +51,12 @@ public class FinancialCalculator {
         double contributionResult = 0;
 
         if (depositType == 1) {
-            contributionResult = calculateSimplePercentFunction(depositAmount, 0.06, period);
+            contributionResult = calculateSimplePercent(depositAmount, yearRate, period);
         } else if (depositType == 2) {
-            contributionResult = calculateComplexPercentFunction(depositAmount, 0.06, period);
+            contributionResult = calculateComplexPercent(depositAmount, yearRate, period);
         }
-        System.out.println("Результат вклада: " + depositAmount + " за " + period + " лет превратятся в " + contributionResult);
-    }
-
-    public static void main(String[] args) {
-        new FinancialCalculator().calculateContributionResult();
+        System.out.println("Результат вклада: " + depositAmount + " за " + period + " лет превратятся в "
+                + contributionResult);
     }
 
 }
